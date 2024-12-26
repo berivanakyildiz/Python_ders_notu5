661b4a9ec7f6648b7f78eb6e072e045b6fd0881f
Berivan AKYILDIZ <berivan.akyildiz0@gmail.com>
2024-12-26 17:36:17 +0300

    Add files via upload

---
+++ 4_format.py
@@ -0,0 +1,14 @@
+
+# Format():Bir dizede belirtilen değerleri formatlar
+
+name = "Berivan"
+surname = "AKYILDIZ"
+age = 22
+
+print("My name is {} {} and I'm {} years old.".format(name,surname,age)) 
+# veya daha kısa yazmak istersek;
+# print(f"My name is {name} {surname} and I'm {age} years old.")
+
+#-------------------------------
+result = 200/700
+print("The result is {}".format(result))
